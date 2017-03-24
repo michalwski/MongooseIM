33ea067b2c112ac4406e53dbcd62c5b5734eddea
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2017-03-24 10:00:49 +0100

    revert pass types

--- apps/ejabberd/priv/mssql2012.sql
+++ apps/ejabberd/priv/mssql2012.sql
@@ -289,8 +289,8 @@ SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[users](
 	[username] [varchar](250) NOT NULL,
-	[password] [varchar](1000) NOT NULL,
-	[pass_details] [varchar](1000) NULL,
+	[password] [varchar](max) NOT NULL,
+	[pass_details] [varchar](max) NULL,
 	[created_at] [datetime] NOT NULL,
  CONSTRAINT [PK_users_username] PRIMARY KEY CLUSTERED
 (
